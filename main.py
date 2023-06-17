befb8bd5a837c1e67c9d484ce1fef56556995a31
Pawel Plociennik <p.plociennik@wp.pl>
2023-06-17 11:18:49 +0200

    Wprowadzenie zmian z zastosowaniem sys.arg

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
 import csv
+
+print(sys.argv)
 
 file_path = "in.csv"
 target_file_path = "out.csv"
@@ -10,16 +13,20 @@ with open(file_path) as plik:
    for line in reader:
        content.append(line)
 
-print(content)
+print('Dane z pliku "in.csv"')
+for row in content:
+    print(row)
 
-lista_1 = content[0]
-lista_1[0] = "gitara"
-lista_2 = content[1]
-lista_2[3] = "kubek"
-lista_3 = content[2]
-lista_3[1] = "17"
-lista_4 = content[3]
-lista_4[3] = "0"
+
+content[int(sys.argv[3])][int(sys.argv[4])] = sys.argv[5]
+content[int(sys.argv[7])][int(sys.argv[6])] = sys.argv[8]
+content[int(sys.argv[10])][int(sys.argv[9])] = sys.argv[11]
+content[int(sys.argv[13])][int(sys.argv[12])] = sys.argv[14]
+
+
+print('\n\nDane do pliku "out.csv"')
+for row in content:
+    print(row)
 
 with open(target_file_path, "w", newline = "") as plik:
     writer = csv.writer(plik)
